ffa39bd29a10c6444f2943547a10c9a8fdd3b132
Your jeongmu <105642388+jeongmuya@users.noreply.github.com>
2025-07-21 23:14:49 +0900

    11

---
+++ MyMemory/MemoCell.swift
@@ -0,0 +1,22 @@
+//
+//  MemoCell.swift
+//  MyMemory
+//
+//  Created by YangJeongMu on 7/19/25.
+//
+
+import UIKit
+
+class MemoCell: UITableViewCell {
+
+    @IBOutlet var img: UIImageView!
+    
+    @IBOutlet var subject: UILabel!
+    
+    @IBOutlet var contents: UILabel!
+    
+    @IBOutlet var regdate: UILabel!
+    
+    
+    
+}
---
+++ MyMemory/MemoData.swift
@@ -0,0 +1,17 @@
+//
+//  MemoData.swift
+//  MyMemory
+//
+//  Created by YangJeongMu on 7/19/25.
+//
+
+import UIKit
+
+class MemoData {
+    
+    var memoIdx: Int?
+    var title: String?
+    var contents: String?
+    var image: UIImage?
+    var regdate: Date?
+}
